300144892d289e560f71f9f00a55c85eec7d8467
miqela97 <132955511+miqela97@users.noreply.github.com>
2023-08-02 11:55:34 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const element = document.getElementById("main");
 element.remove();
-const newHeader = document.querySelector('h1');
-newHeader.id = 'victory';
-const yourName = 'Your Name';
-newHeader.textContent = `${yourName} is the champion`;
+const newHeader = document.createElement ("h1")
+newHeader.id = "victory"
+newHeader.textContent = "giorgi is the champion";
+
+document.body.appendChild(newHeader)
